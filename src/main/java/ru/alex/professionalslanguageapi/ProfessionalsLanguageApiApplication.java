39514c9198cacc59844654cc8121781e7690125a
Alex <manyakinsasha@gmail.com>
2025-03-26 13:42:06 +0300

    initial commit

---
+++ src/main/java/ru/alex/professionalslanguageapi/ProfessionalsLanguageApiApplication.java
@@ -0,0 +1,13 @@
+package ru.alex.professionalslanguageapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessionalsLanguageApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfessionalsLanguageApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/alex/professionalslanguageapi/ProfessionalsLanguageApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.alex.professionalslanguageapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessionalsLanguageApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
